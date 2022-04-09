7bc1929c65989720182184743e09ebb8f21eb9a2
yooki22 <yooki22@pos.idserve.net>
2022-04-10 01:40:24 +0900

    Initial commit

---
+++ WikiMap/ComentView.swift
@@ -0,0 +1,20 @@
+//
+//  ComentView.swift
+//  WikiMap
+//
+//  Created by 기윤수 on 2022/04/10.
+//
+
+import SwiftUI
+
+struct ComentView: View {
+    var body: some View {
+        Text("Hello, hi!")
+    }
+}
+
+struct ComentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ComentView()
+    }
+}
