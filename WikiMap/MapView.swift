d33b732cc580fdcb5527ff19d20a2503a7fa1d2a
한형서 <2017029243@hanyang.ac.kr>
2022-04-11 17:36:29 +0900

    Feat(Whole Map Render, Random Pin Generator)

--- WikiMap/MapView.swift
+++ WikiMap/MapView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MapView.swift
 //  WikiMap
 //
 //  Created by Hyung Seo Han on 2022/04/05.
@@ -9,38 +9,77 @@ import SwiftUI
 import MapKit
 import CoreLocationUI
 
+struct Location: Identifiable{
+    var id = UUID()
+    var title: String
+    var latitude: Double
+    var longitude: Double
+}
+
+extension Location{
+    static var pinLocations = [
+        Location(title: "Test 0", latitude: 35.500000, longitude: 40.000000)
+    ]
+}
 
 struct MapView: View {
     @State private var showModal = false
     @StateObject private var viewModel = MapViewModel()
     
     var body: some View {
-        ZStack(alignment: .bottom){
-                
+        ZStack(alignment: .topLeading){
+            
+            
             //Map을 사용해서 위에서 설정한 region 바인딩 진행
             Map(coordinateRegion: $viewModel.region,
                 interactionModes: .all,
-                showsUserLocation: true)
+                showsUserLocation: true,
+                annotationItems: Location.pinLocations){
+                MapMarker(coordinate: CLLocationCoordinate2D(latitude: $0.latitude, longitude: $0.longitude), tint: .blue)
+            }
                 .ignoresSafeArea()
                 .tint(.pink)
                 
+            
             //Text와 Button은 동등한 선상에 있기 때문에 VStack으로 설정함
+            /*Text("위도 : \(viewModel.region.center.latitude) ,경도: \(viewModel.region.center.longitude),  Zoom:\(viewModel.region.span.latitudeDelta) ")
+                .font(.caption)
+                .padding(100)
+            */
+            
             VStack{
-                Text("위도 : \(viewModel.region.center.latitude) ,경도: \(viewModel.region.center.longitude),  Zoom:\(viewModel.region.span.latitudeDelta) ")
-                    .font(.caption)
-                    .padding()
-                        
-                    
                 LocationButton(.currentLocation){
                     viewModel.requestAllowOnceLocationPermission()
                 }
                 .foregroundColor(.white)
                 .cornerRadius(10)
                 .symbolVariant(.fill)
+                .labelStyle(.iconOnly)
                 .tint(.blue)
+                .padding(EdgeInsets(top: 0, leading: 10 , bottom: 0, trailing: 0))
                 
-            }.statusBar(hidden: true)//위의 상단바(시간이랑 배터리 삭제)
+                
+                
+                
+                Button("Pin",action: viewModel.addAnnotationPin)
+                    .frame(width: 41, height: 41)
+                    .foregroundColor(Color.white)
+                    .background(Color.green)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .padding(EdgeInsets(top: 0, leading: 10 , bottom: 0, trailing: 0))
+                
+                Button("Whole",action: viewModel.wholeMap)
+                    .font(.system(size:10, weight: .bold))
+                    .frame(width: 41, height: 41)
+                    .foregroundColor(Color.white)
+                    .background(Color.black)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .padding(EdgeInsets(top: 0, leading: 10 , bottom: 0, trailing: 0))
 
+            }.statusBar(hidden: true)//위의 상단바(시간이랑 배터리 삭제)
+             .padding(EdgeInsets(top: 60, leading: 0 , bottom: 0, trailing: 0))
+            //공돌이 만든 ModalView
+            ModalView()
         }
     }
 }
@@ -59,7 +98,11 @@ final class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate{
     @Published var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: MapDefaults.latitude, longitude: MapDefaults.longitude) ,
         span: MKCoordinateSpan(latitudeDelta: MapDefaults.zoom, longitudeDelta: MapDefaults.zoom))
-        
+    
+    @Published var pinsLocations: [Location] = Location.pinLocations
+    
+    private var test = 0
+    
     private enum MapDefaults{
         static let latitude =  40.00011
         static let longitude = 131.52104
@@ -85,8 +128,8 @@ final class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate{
             //show an error
             return
         }
-        DispatchQueue.main.async{
-            self.region = MKCoordinateRegion(
+        DispatchQueue.main.async{[weak self] in
+            self?.region = MKCoordinateRegion(
                 center: latestLocation.coordinate,
                 span: MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001))
                                                 
@@ -96,4 +139,27 @@ final class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate{
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print(error.localizedDescription)
     }
+    
+    func addAnnotationPin(){
+        test = test + 1
+        let testTitle = "Test "+String(test)
+        let testLatitude = round(Double.random(in: 1...90)*100000)/100000
+        let testLongitude = round(Double.random(in: 1...135)*100000/100000)
+        
+        Location.pinLocations.append(Location(title: testTitle, latitude: testLatitude, longitude: testLongitude))
+        
+        DispatchQueue.main.async {[weak self] in
+            self?.pinsLocations = Location.pinLocations
+            
+        }
+    }
+    
+    func wholeMap(){
+        DispatchQueue.main.async{[weak self] in
+            self?.region = MKCoordinateRegion(
+                center: CLLocationCoordinate2D(latitude: 27.83, longitude: 84.69),
+                span: MKCoordinateSpan(latitudeDelta: 155.0, longitudeDelta: 155.0))
+        }
+    }
 }
+
