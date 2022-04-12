2d4ffa8c9f88e73b3a404e2347a2d55a648e5ab8
Pos-Felix <wonyang22@pos.idserve.net>
2022-04-12 15:55:36 +0900

    Modified the code. (Not completed)

--- WikiMap/Test.swift
+++ WikiMap/Test.swift
@@ -10,62 +10,76 @@ import UIKit
 
 struct TestView: View {
     
+    @ObservedObject var imageStack = ImageStack()
+    
     @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
     @State private var selectedImage: UIImage?
     @State private var isImagePickerDisplay = false
     
     var body: some View {
         
-        NavigationView {
-            VStack(spacing: 10){
-                HStack{
-                    Spacer().frame(width: 10)
-                    Text("사진")
-                        .font(.system(size: 20))
-                    Spacer()
-                }
-                Spacer().frame(height: 10)
+        VStack(spacing: 10){
+            HStack{
+                Spacer().frame(width: 10)
+                Text("사진")
+                    .font(.system(size: 20))
+                Spacer()
+            }
+            Spacer().frame(height: 10)
+            
+            HStack{
+                Spacer().frame(width: 10)
                 
-                HStack{
-                    Spacer().frame(width: 10)
-                    ScrollView(.horizontal, showsIndicators: false){
-                      if selectedImage != nil {
-                        Image(uiImage: selectedImage!)
-                            .resizable()
-                            .frame(width: 85, height: 85)
-                            .scaledToFit()
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                      } else {
-                        Image(systemName: "plus.square")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 85, height: 85)
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                      }
+                
+                
+                ScrollView(.horizontal, showsIndicators: false){
+                    HStack(spacing: 10){
+                        ForEach(0 ..< 10){i in
+                            if selectedImage != nil {
+                                Image(uiImage: selectedImage!)
+                                    .resizable()
+                                    .frame(width: 85, height: 85)
+                                    .scaledToFit()
+                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                            } else {
+                                Image(systemName: "plus.square")
+                                    .resizable()
+                                    .frame(width: 85, height: 85)
+                                    .scaledToFit()
+                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                            }
+                        }
+                        
                     }
                 }
-                
-                
-                
-                
-                Button("Camera") {
-                    self.sourceType = .camera
-                    self.isImagePickerDisplay.toggle()
-                }
-                .padding(3)
-                
-                Button("Photo") {
-                    self.sourceType = .photoLibrary
-                    self.isImagePickerDisplay.toggle()
-                }
-            }
-            .sheet(isPresented: self.$isImagePickerDisplay) {
-                ImagePickerView(selectedImage: self.$selectedImage, sourceType: self.sourceType)
             }
             
+            
+            Button("Camera") {
+                self.imageStack.count += 1
+                self.sourceType = .camera
+                self.isImagePickerDisplay.toggle()
+            }
+            .padding(3)
+            
+            Button("Photo") {
+                self.imageStack.count += 1
+                self.sourceType = .photoLibrary
+                self.isImagePickerDisplay.toggle()
+            }
         }
+        .sheet(isPresented: self.$isImagePickerDisplay) {
+            ImagePickerView(selectedImage: self.$selectedImage, sourceType: self.sourceType)
+            
+        }
+        
+        
+        
+        
     }
 }
+
+
 
 struct TestView_Previews: PreviewProvider {
     static var previews: some View {
