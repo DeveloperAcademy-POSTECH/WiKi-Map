ba8b14b584626c3a81e9ffb294723137201bc922
한형서 <2017029243@hanyang.ac.kr>
2022-04-15 14:50:16 +0900

    Added Multiple annotation pin by gesture recognizer

--- WikiMap/MapAnnotationView.swift
+++ WikiMap/MapAnnotationView.swift
@@ -9,43 +9,64 @@ import SwiftUI
 import UIKit
 import MapKit
 
+
+extension MKPointAnnotation{
+    convenience init(title: String, coordinate : CLLocationCoordinate2D){
+        self.init()
+        self.title = title
+        self.coordinate = coordinate
+    }
+}
+
 struct MapAnnotationView: UIViewRepresentable {
-    //Mock Data
-    let landmarks = LandmarkAnnotation.requestMockData()
+    @Binding var pins : [MKPointAnnotation]
     
+    //Make Coordinator
     func makeCoordinator() -> MapAnnotationViewCoordinator{
         MapAnnotationViewCoordinator(self)
     }
     
     //Replace body : some View -> makeUIView
     func makeUIView(context: Context) -> MKMapView {
+        //지도 설정
         let map = MKMapView(frame: .zero)
         map.delegate = context.coordinator
-        let longPressed = UILongPressGestureRecognizer(target: context.coordinator, action: #selector(context.coordinator.test(_:)))
-        longPressed.minimumPressDuration = 0.5
+        //제스처 인식
+        let longPressed = UILongPressGestureRecognizer(target: context.coordinator, action: #selector(context.coordinator.test(_:viewFor:_:)))
+        longPressed.minimumPressDuration = 2.0
         map.addGestureRecognizer(longPressed)
         return map
     }
     
     //context: structure containing information about the current state of the system.
     func updateUIView(_ uiView: MKMapView, context: Context) {
-        uiView.delegate = context.coordinator
-        
-        //Annotation Pin Setting
-        for location in landmarks{
-            let annotation = MKPointAnnotation()
-            annotation.title = location["title"] as? String
-            annotation.subtitle = location["subtitle"] as? String
-            annotation.coordinate = CLLocationCoordinate2D(latitude: location["latitude"] as! Double, longitude: location["longitude"] as! Double)
-            uiView.addAnnotation(annotation)
+        //uiView.delegate = context.coordinator
+        let count = uiView.annotations
+        if count != nil{
+            uiView.removeAnnotations(uiView.annotations)
         }
+        uiView.addAnnotations(pins)
+        
+        //View Update에 대한 방아쇠를 당겨야한다고 함. -> 직접적으로 업데이트를 해줘야한다.
     }
-    
 }
 
-//closer
-struct MapAnnotationView_Previews: PreviewProvider {
-    static var previews: some View {
-        MapAnnotationView()
+
+
+class AnnotationViewModel: ObservableObject{
+    @Published var pins = [MKPointAnnotation]()
+    init() {
+        var annotation = MKPointAnnotation()
+        annotation.title = "Apple Store"
+        annotation.subtitle = "shibural"
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 40.77, longitude: -73.98)
+        pins.append(annotation)
+
+        annotation = MKPointAnnotation()
+        annotation.title = "Shoe Store"
+        annotation.subtitle = "shibural"
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 40.78, longitude: -73.98)
+        pins.append(annotation)
     }
 }
+
